9108ea6d2e2682a22656fb407674082356eec6cb
Thomas Smith <thomasfn@fortfn.co.uk>
2019-06-30 17:49:19 +0100

    Switched entity parser over to new VKV.

--- Source/HL2Editor/Private/EntityParser.cpp
+++ Source/HL2Editor/Private/EntityParser.cpp
@@ -4,145 +4,20 @@
 
 #include "Internationalization/Regex.h"
 
-DEFINE_LOG_CATEGORY(LogHL2EntityParser);
-
-const FString FEntityParser::entityParserTokenTypeNames[] =
-{
-	TEXT("Whitespace"),
-	TEXT("OpenGroup"),
-	TEXT("CloseGroup"),
-	TEXT("String")
-};
-
-bool FEntityParser::Tokenise(const FString& src, TArray<FEntityParserToken>& out)
-{
-	const static FRegexPattern patterns[] =
-	{
-		FRegexPattern(TEXT("[\\s\\n\\r]+")), // Whitespace
-		FRegexPattern(TEXT("\\{")), // OpenGroup
-		FRegexPattern(TEXT("\\}")), // CloseGroup
-		FRegexPattern(TEXT("\"((?:\\\\\"|[^\"])*)\"")) // String
-	};
-	constexpr int numPatterns = sizeof(patterns) / sizeof(FRegexPattern);
-	static_assert(numPatterns == (int)EEntityParserTokenType::ETokenType_Count, "EntityParser.cpp: Each token must have a regex pattern");
-	int curPos = 0;
-	while (curPos < src.Len())
-	{
-		bool matched = false;
-		for (int i = 0; i < numPatterns; ++i)
-		{
-			FRegexMatcher matcher(patterns[i], src);
-			matcher.SetLimits(curPos, src.Len());
-			if (matcher.FindNext() && matcher.GetMatchBeginning() == curPos)
-			{
-				matched = true;
-				if (i > 0) // ignore whitespace
-				{
-					FEntityParserToken token;
-					token.type = (EEntityParserTokenType)i;
-					token.start = curPos;
-					token.end = matcher.GetMatchEnding();
-					out.Add(token);
-				}
-				curPos = matcher.GetMatchEnding();
-				break;
-			}
-		}
-		if (!matched)
-		{
-			UE_LOG(LogHL2EntityParser, Error, TEXT("Unexpected '%c' at %d"), src[curPos], curPos);
-			return false;
-		}
-	}
-	return true;
-}
-
-void FEntityParser::ReadToken(const FString& src, const FEntityParserToken& token, FString& out)
-{
-	out = FString(token.end - token.start, *src + token.start);
-}
-
-bool FEntityParser::ParseGroup(const FString& src, const TArray<FEntityParserToken>& tokens, int& nextToken, TMap<FName, FString>& outKeyValues, TArray<FEntityLogicOutput>& outLogicOutputs)
-{
-	// OpenGroup
-	if (tokens[nextToken++].type != EEntityParserTokenType::OpenGroup) { return false; }
-
-	// String | CloseGroup
-	while (tokens[nextToken].type != EEntityParserTokenType::CloseGroup)
-	{
-		// String
-		const FEntityParserToken& keyToken = tokens[nextToken++];
-		if (keyToken.type != EEntityParserTokenType::String)
-		{
-			UE_LOG(LogHL2EntityParser, Error, TEXT("Expected 'String', got '%s' at %d"), *entityParserTokenTypeNames[(int)keyToken.type], keyToken.start);
-			return false;
-		}
-
-		// String
-		const FEntityParserToken& valueToken = tokens[nextToken++];
-		if (valueToken.type != EEntityParserTokenType::String)
-		{
-			UE_LOG(LogHL2EntityParser, Error, TEXT("Expected 'String', got '%s' at %d"), *entityParserTokenTypeNames[(int)valueToken.type], valueToken.start);
-			return false;
-		}
-
-		FString keyStr, valueStr;
-		ReadToken(src, keyToken, keyStr);
-		keyStr.RemoveFromStart(TEXT("\""));
-		keyStr.RemoveFromEnd(TEXT("\""));
-		ReadToken(src, valueToken, valueStr);
-		valueStr.RemoveFromStart(TEXT("\""));
-		valueStr.RemoveFromEnd(TEXT("\""));
-
-		// Check logic output
-		TArray<FString> logicArgs;
-		valueStr.ParseIntoArray(logicArgs, TEXT(","), false);
-		if (logicArgs.Num() >= 4)
-		{
-			FEntityLogicOutput logicOutput;
-			logicArgs[0].TrimEndInline();
-			logicArgs[0].TrimStartInline();
-			logicOutput.TargetName = FName(*logicArgs[0]);
-			logicOutput.OutputName = FName(*keyStr);
-			logicArgs[1].TrimEndInline();
-			logicArgs[1].TrimStartInline();
-			logicOutput.InputName = FName(*logicArgs[1]);
-			logicOutput.Delay = FCString::Atof(*logicArgs.Last(1));
-			logicOutput.Once = FCString::Atoi(*logicArgs.Last(0)) != -1;
-			logicArgs.RemoveAt(logicArgs.Num() - 2, 2);
-			logicArgs.RemoveAt(0, 2);
-			logicOutput.Params = logicArgs;
-			outLogicOutputs.Add(logicOutput);
-		}
-		else
-		{
-			outKeyValues.Add(FName(*keyStr), valueStr);
-		}
-	}
-
-	// CloseGroup
-	nextToken++;
-
-	return true;
-}
-
 bool FEntityParser::ParseEntities(const FString& src, TArray<FHL2EntityData>& out)
 {
-	TArray<FEntityParserToken> tokens;
-	if (!Tokenise(src, tokens))
-	{
-		UE_LOG(LogHL2EntityParser, Error, TEXT("Failed to tokenise entity string"));
-		return false;
-	}
+	const UValveDocument* document = UValveDocument::Parse(src);
+	if (document == nullptr) { return false; }
+
+	const UValveArrayValue* arrayValue = CastChecked<UValveArrayValue>(document->Root);
+
 	TMap<FName, FString> keyValues;
 	TArray<FEntityLogicOutput> logicOutputs;
-	int nextToken = 0;
-	while (nextToken < tokens.Num())
+	for (const UValveValue* value : arrayValue->Items)
 	{
-		int tmp = nextToken;
-		if (ParseGroup(src, tokens, tmp, keyValues, logicOutputs))
+		const UValveGroupValue* groupValue = CastChecked<UValveGroupValue>(value);
+		if (ParseGroup(groupValue, keyValues, logicOutputs))
 		{
-			nextToken = tmp;
 			FHL2EntityData entity;
 			entity.KeyValues = keyValues;
 			entity.LogicOutputs = logicOutputs;
@@ -153,11 +28,48 @@ bool FEntityParser::ParseEntities(const FString& src, TArray<FHL2EntityData>& ou
 		}
 		else
 		{
-			const FEntityParserToken& token = tokens[nextToken];
-			UE_LOG(LogHL2EntityParser, Error, TEXT("Unexpected token '%s' at %d"), *entityParserTokenTypeNames[(int)token.type], token.start);
 			return false;
 		}
 	}
+	return true;
+}
+
+bool FEntityParser::ParseGroup(const UValveGroupValue* group, TMap<FName, FString>& outKeyValues, TArray<FEntityLogicOutput>& outLogicOutputs)
+{
+	for (const FValveGroupKeyValue& kv : group->Items)
+	{
+		const UValvePrimitiveValue* primitiveValue = Cast<UValvePrimitiveValue>(kv.Value);
+		if (primitiveValue != nullptr)
+		{
+			const FString value = primitiveValue->AsString();
+
+			// Check logic output
+			TArray<FString> logicArgs;
+			value.ParseIntoArray(logicArgs, TEXT(","), false);
+			if (logicArgs.Num() >= 4)
+			{
+				FEntityLogicOutput logicOutput;
+				logicArgs[0].TrimEndInline();
+				logicArgs[0].TrimStartInline();
+				logicOutput.TargetName = FName(*logicArgs[0]);
+				logicOutput.OutputName = kv.Key;
+				logicArgs[1].TrimEndInline();
+				logicArgs[1].TrimStartInline();
+				logicOutput.InputName = FName(*logicArgs[1]);
+				logicOutput.Delay = FCString::Atof(*logicArgs.Last(1));
+				logicOutput.Once = FCString::Atoi(*logicArgs.Last(0)) != -1;
+				logicArgs.RemoveAt(logicArgs.Num() - 2, 2);
+				logicArgs.RemoveAt(0, 2);
+				logicOutput.Params = logicArgs;
+				outLogicOutputs.Add(logicOutput);
+			}
+			else
+			{
+				outKeyValues.Add(kv.Key, value);
+			}
+		}
+	}
+
 	return true;
 }
 
--- Source/HL2Editor/Private/EntityParser.h
+++ Source/HL2Editor/Private/EntityParser.h
@@ -2,34 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "HL2EntityData.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogHL2EntityParser, Log, All);
-
-enum class EEntityParserTokenType
-{
-	Whitespace,
-	OpenGroup,
-	CloseGroup,
-	String,
-	ETokenType_Count
-};
-
-struct FEntityParserToken
-{
-	EEntityParserTokenType type;
-	int start;
-	int end;
-};
+#include "ValveKeyValues.h"
 
 class FEntityParser
 {
 private:
 
 	FEntityParser();
-
-private:
-
-	static const FString entityParserTokenTypeNames[(int)EEntityParserTokenType::ETokenType_Count];
 	
 public:
 
@@ -40,11 +19,7 @@ public:
 
 private:
 
-	static bool Tokenise(const FString& src, TArray<FEntityParserToken>& out);
-
-	static void ReadToken(const FString& src, const FEntityParserToken& token, FString& out);
-
-	static bool ParseGroup(const FString& src, const TArray<FEntityParserToken>& tokens, int& nextToken, TMap<FName, FString>& outKeyValues, TArray<FEntityLogicOutput>& outLogicOutputs);
+	static bool ParseGroup(const UValveGroupValue* group, TMap<FName, FString>& outKeyValues, TArray<FEntityLogicOutput>& outLogicOutputs);
 
 	inline static void ParseCommonKeys(FHL2EntityData& entity);
 };
